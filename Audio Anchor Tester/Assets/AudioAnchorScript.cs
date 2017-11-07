9b3a4b95b8e47f70351d8817473836a11c048213
Elisha <elisha.sarkis96@gmail.com>
2017-11-07 18:14:50 -0500

    Add Music looping and audio preservation between scenes option

--- Audio Anchor Tester/Assets/AudioAnchorScript.cs
+++ Audio Anchor Tester/Assets/AudioAnchorScript.cs
@@ -8,6 +8,9 @@ public class AudioAnchorScript : MonoBehaviour
 	public static AudioAnchorScript inst = null;	// the singleton instance
 	[HideInInspector] public AudioSource source;	// AudioSource component
 
+	public bool playSoundEffectsBetweenScenes = true;
+	public bool playMusicBetweenScenes = true;
+
 	// In-editor dictionary pair AudioClip->float equivalent
 	[System.Serializable]
     public struct ClipIntensityPair
@@ -16,12 +19,16 @@ public class AudioAnchorScript : MonoBehaviour
         public float intensity;
     }
 	public ClipIntensityPair[] ClipIntensityPairArray;
+	public ClipIntensityPair[] MusicIntensityPairArray;
 
 
 	void Awake () 
 	{
 		InitializeInstance();
 		source = GetComponent<AudioSource>();
+
+		// For music/audio transitions between scenes
+		if (playSoundEffectsBetweenScenes || playMusicBetweenScenes) DontDestroyOnLoad(this.gameObject);
 	}
 
 
@@ -35,10 +42,26 @@ public class AudioAnchorScript : MonoBehaviour
                 source.PlayOneShot(ClipIntensityPairArray[i].clip, ClipIntensityPairArray[i].intensity);
                 return;
             }
-			
 		}
-        Debug.LogError("AudioAnchor: " + clipName + " AudioClip name not found. Are you sure it's been added to the AudioAnchorScript component?");
+        Debug.LogError("AudioAnchor: " + clipName
+						+ " AudioClip name not found. Are you sure it's been added to the AudioAnchorScript component Sounds array?");
     }
+
+	// Play looping music via this GameObject's AudioSource via given AudioClip name
+	public void PlayMusic(string musicName)
+	{
+		for (int i = 0; i < MusicIntensityPairArray.Length; i++)
+		{
+			if ( musicName == MusicIntensityPairArray[i].clip.name )
+            {
+				source.clip = MusicIntensityPairArray[i].clip;
+				source.Play();
+				return;
+			}
+			Debug.LogError("AudioAnchor: " + musicName
+							+ " AudioClip name not found. Are you sure it's been added to the AudioAnchorScript component Music array?");
+		}
+	}
 
 
 	// Initialize the instance of this singleton
