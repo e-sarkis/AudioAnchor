db9c87d40250586cc4fd10409dc85890afdeaaef
Elisha <elisha.sarkis96@gmail.com>
2017-11-07 17:31:18 -0500

    Added example scene utilising free-to-use BFXR Coin and Laser sounds and created AudioAnchorTestingScript.cs for use in scene AudioAnchorTest.unity

---
+++ Audio Anchor Tester/Assets/AudioAnchor Example/AudioAnchorTesterScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAnchorTesterScript : MonoBehaviour 
+{
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.A)) AudioAnchorScript.inst.PlaySound("AA_Coin");
+		if (Input.GetKeyDown(KeyCode.S)) AudioAnchorScript.inst.PlaySound("AA_Laser");
+	}
+}
