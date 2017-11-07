3c580a53bbe6fdf49fe30479220ad43f5db676e6
Elisha <elisha.sarkis96@gmail.com>
2017-11-07 16:57:24 -0500

    Create AudioAnchorScript.cs and add singleton functionality

---
+++ Audio Anchor Tester/Assets/AudioAnchorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// AudioAnchor - Audio Management and Playing Singleton
+public class AudioAnchorScript : MonoBehaviour 
+{
+	public static AudioAnchorScript inst = null;	// the singleton instance
+
+	void Awake () 
+	{
+		InitializeInstance();
+	}
+
+	// Initialize the instance of this singleton
+	private void InitializeInstance()
+	{
+		// Delete the older instance if it exists
+		if (inst == null) inst = this;
+        else if (inst != this) Destroy(this.gameObject);
+	}
+}
