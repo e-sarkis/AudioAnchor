db789afd0c727945caef22db530a1529954bf8a9
Elisha <elisha.sarkis96@gmail.com>
2017-11-07 18:06:22 -0500

    Remove redundant Dictionary logic from AudioAnchorScript.cs

--- Audio Anchor Tester/Assets/AudioAnchorScript.cs
+++ Audio Anchor Tester/Assets/AudioAnchorScript.cs
@@ -15,40 +15,30 @@ public class AudioAnchorScript : MonoBehaviour
         public AudioClip clip;
         public float intensity;
     }
-	public ClipIntensityPair[] ClipIntensityPairArray;	// For use in-editor
-	// The true Dicitonary to be used in logic
-	public Dictionary<AudioClip, float> ClipsToIntensities = new Dictionary<AudioClip, float>();
+	public ClipIntensityPair[] ClipIntensityPairArray;
 
 
 	void Awake () 
 	{
 		InitializeInstance();
 		source = GetComponent<AudioSource>();
-		PopulateDictionary();
 	}
 
 
 	// Play a sound via this GameObjects AudioSource via given AudioClip name
 	public void PlaySound(string clipName)
     {
-        foreach (AudioClip clip in ClipsToIntensities.Keys)
-        {
-            if (clipName == clip.name)
+		for (int i = 0; i < ClipIntensityPairArray.Length; i++)
+		{
+			if ( clipName == ClipIntensityPairArray[i].clip.name )
             {
-                source.PlayOneShot(clip, ClipsToIntensities[clip]);
+                source.PlayOneShot(ClipIntensityPairArray[i].clip, ClipIntensityPairArray[i].intensity);
                 return;
             }
-        }
+			
+		}
         Debug.LogError("AudioAnchor: " + clipName + " AudioClip name not found. Are you sure it's been added to the AudioAnchorScript component?");
     }
-
-
-	// Populate ClipsToIntensities via ClipIntensityPairArray for use in logic
-	private void PopulateDictionary()
-	{
-		for (int i = 0; i < ClipIntensityPairArray.Length; i++)
-            ClipsToIntensities.Add(ClipIntensityPairArray[i].clip, ClipIntensityPairArray[i].intensity);
-	}
 
 
 	// Initialize the instance of this singleton
